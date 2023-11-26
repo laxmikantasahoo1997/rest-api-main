1452470bbd3e8686b192d1f10246907297ffac74
laxmikanta1997 <laxmikanta@enhanzed.com>
2023-11-26 11:57:36 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+// app.js
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const stockRoutes = require('./routes/stockRoutes');
+const dotenv = require('dotenv');
+
+dotenv.config(); // Load environment variables from .env file
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors({
+  origin:["https://deploy-mern-1whq.vercel.app"],
+  methods:["POST","GET"],
+  credentials:true
+}));
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Use stock routes
+app.use('/api', stockRoutes);
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
